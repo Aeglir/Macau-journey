2e39e4c6017de837f94dba2841cefbe9452dfaec
MFk <1814569697@qq.com>
2021-11-02 18:54:17 +0800

    save

--- Assets/script/MainMenuButton.cs
+++ Assets/script/MainMenuButton.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
@@ -11,38 +12,21 @@ namespace MainMenu
         {
             base.Awake();
             //设置回调函数
-            settingListerners(StartButtonClicked, ContinueButtonClicked, ConfigButtonClicked, ExitButtonClicked);
+            settingListerners(() =>
+            {
+                Button sourceButton = EventSystem.current.currentSelectedGameObject.GetComponent<Button>();
+                StartCoroutine(WAIT.onWaiting(1.0f, () => { SceneManager.LoadScene("GameScene"); }));
+            }, () =>
+            {
+                StartCoroutine(WAIT.onWaiting(1.0f, () => { SceneManager.LoadScene("ContinueScene"); }));
+            }, () =>
+            {
+                StartCoroutine(WAIT.onWaiting(1.0f, () => { SceneManager.LoadScene("ConfigScene"); }));
+            }, () =>
+            {
+                StartCoroutine(WAIT.onWaiting(1.0f, () => { Application.Quit(); }));
+            });
 
-        }
-        new private void Start()
-        {
-            base.Start();
-        }
-
-        private void SwitchScene(string sceneName){
-            Invoke("SwitchToGameScene",1);
-        }
-
-        void StartButtonClicked()
-        {
-            //切换至游戏场景
-            Button button = getSourceButton(StartButtonClicked);
-            Invoke("SwitchToGameScene",1);
-        }
-        void ContinueButtonClicked()
-        {
-            //打开读档界面
-            SceneManager.LoadScene("ContinueScene");
-        }
-        void ConfigButtonClicked()
-        {
-            //打开设置界面
-            SceneManager.LoadScene("ConfigScene");
-        }
-        void ExitButtonClicked()
-        {
-            //退出应用
-            Application.Quit();
         }
     }
 }
